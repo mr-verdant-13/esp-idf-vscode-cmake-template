58cdde422c13090c27f6fe00a5e32507e3114b25
Karlo Verde <karlo.verde.1996@gmail.com>
2019-07-31 16:21:41 -0500

    Bat files for tasks

--- scripts/InitializePrompt.bat
+++ scripts/InitializePrompt.bat
@@ -1,8 +1,8 @@
 @echo off
-cd "C:\esp-idf"
-call C:\.espressif\idf_cmd_init.bat %localappdata%\Programs\Python\Python37 %programfiles(x86)%\Git\cmd %programfiles%\Git\cmd
-cd "%workspaceFolder%"
-cls
+cd /d "C:\esp-idf"
+call C:\.espressif\idf_cmd_init.bat "%localappdata%\Programs\Python\Python37" "%programfiles(x86)%\Git\cmd" "%programfiles%\Git\cmd"
+cd /d "%workspaceFolder%"
+REM cls
 echo:
 echo:'idf.py' ENABLED
 echo:'idf.py' HABILITADO
